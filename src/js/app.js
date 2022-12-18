e00ff6f83a7f04f9bf44467194c044c155ed359c
Valera <bogatyrev.den06@gmail.com>
2022-12-18 23:35:05 +0300

    update

--- src/js/app.js
+++ src/js/app.js
@@ -14,3 +14,47 @@ import {
 import AOS from 'aos';
 
 flsFunctions.isWebp();
+
+const swiper = new Swiper('.auctions-slider', {
+	loop: false,
+	navigation: {
+		nextEl: '.swiper-button-next1',
+		prevEl: '.swiper-button-prev1'
+	},
+	modules: [Navigation, FreeMode],
+	autoHeight: true,
+	speed: 500,
+	slidesPerView: 3,
+	spaceBetween: 42,
+	freeMode: true,
+	grabCursor: true,
+});
+const swiper2 = new Swiper('.popular-slider', {
+	loop: false,
+	navigation: {
+		nextEl: '.swiper-button-next2',
+		prevEl: '.swiper-button-prev2'
+	},
+	modules: [Navigation, FreeMode],
+	autoHeight: true,
+	speed: 500,
+	slidesPerView: 2.5,
+	spaceBetween: 42,
+	freeMode: true,
+	grabCursor: true,
+});
+
+const swiper3 = new Swiper('.categories-slider', {
+	loop: false,
+	navigation: {
+		nextEl: '.swiper-button-next3',
+		prevEl: '.swiper-button-prev3'
+	},
+	modules: [Navigation, FreeMode],
+	autoHeight: true,
+	speed: 500,
+	slidesPerView: 2.5,
+	spaceBetween: 42,
+	freeMode: true,
+	grabCursor: true,
+});
